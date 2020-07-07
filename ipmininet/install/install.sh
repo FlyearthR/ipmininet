566f764db9eed2e20fbc9063b62742495bbd6545
Tom Rousseaux <rousseaux1.linerider@gmail.com>
2020-07-07 02:33:38 +0200

    revert modification to install.sh

--- ipmininet/install/install.sh
+++ ipmininet/install/install.sh
@@ -34,9 +34,7 @@ if [ "$ARCH" = "x86_64" ]; then ARCH="amd64"; fi
 if [ "$ARCH" = "i686" ]; then ARCH="i386"; fi
 
 test -e /etc/debian_version && DIST="Debian"
-if test -e /etc/lsb-release; then
-    grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
-fi
+grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
 if [ "$DIST" = "Ubuntu" ] || [ "$DIST" = "Debian" ]; then
     # Truly non-interactive apt-get installation
     install='sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install'
